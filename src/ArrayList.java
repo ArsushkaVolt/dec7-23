3ea8b3c089eb25a38992a1937377c087ae0e9b2d
student <senyasmurf@gmail.com>
2023-12-12 20:50:41 +0300

    arraylistbase

--- src/ArrayList.java
+++ src/ArrayList.java
@@ -16,12 +16,13 @@ public class ArrayList
         Count = 1;
     }
 
-    public ArrayList(int[] element) // конструктор для строк
+    public ArrayList(int[] element)
     {
         _array = new int[(int)(element.length * 1.5)];
         System.arraycopy(element, 0, _array, 0, element.length);
         Count = element.length;
     }
+
     public int Get(int index)
     {
         if(index < 0 || index >= Count)
@@ -42,13 +43,33 @@ public class ArrayList
         Count ++;
 
     }
+    public void Add(int[] elements)
+    {
+        if(_array.length - Count < elements.length)
+        {
+            IncreaseLength(elements.length);
+
+        }
+
+        for(int i = Count; i < elements.length; i++)
+        {
+            _array[Count + i] = elements[i];
+            Count++;
+        }
+
+
+    }
     private  void IncreaseLength(int elementCount)
     {
         int newLength = (int) ((_array.length + elementCount) * 1.5 );
         int[] newArray = new int[newLength];
         System.arraycopy(_array, 0, newArray, 0, _array.length);
     }
+
+
 }
+
+
 
 //1)проверяем можем ли вы добавить элемент
 //2) если места нету, вызываем метод увеличения длины массива
